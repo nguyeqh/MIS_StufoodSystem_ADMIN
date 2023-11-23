e17776431c1a58f6afa802f77d91fc80c7be773e
Nguyễn Quỳnh Như <104415043+nguyeqh@users.noreply.github.com>
2023-11-23 19:17:36 +0700

    add list view employee

---
+++ StufoodSystem_ADMIN/SEFinalProject_Winform/Controllers/EmployeeController.cs
@@ -0,0 +1,73 @@
+﻿using StufoodSystem_ADMIN.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StufoodSystem_ADMIN.Controllers
+{
+    public class EmployeeController
+    {
+        private static String strConn = ConfigurationManager.ConnectionStrings["MySQLConnection"].ConnectionString;
+        private static SqlConnection conn;
+        public EmployeeController() 
+        {
+            conn = new SqlConnection(strConn);
+            
+        }
+        public static List<Employee> GetAllEmployee()
+        {
+            conn = new SqlConnection(strConn);
+            conn.Open();
+            String sSQL = "SELECT * FROM EMPLOYEE";
+            List<Employee> employees = GetEmployeesFromDatabase(strConn, sSQL);
+      
+            conn.Close();
+            return employees;
+
+        }
+
+        //--------------- PRIVATE FUNCTION -------------------- //
+
+        static List<Employee> GetEmployeesFromDatabase(string connectionString, string sSQL)
+        {
+            List<Employee> result = new List<Employee>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SqlDataAdapter adapter = new SqlDataAdapter(sSQL, connection))
+                {
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+
+                    // Convert DataTable rows to List<Employee>
+                    foreach (DataRow row in dataTable.Rows)
+                    {
+                        Employee employee = new Employee
+                        {
+                            employeeID = row["employeeID"].ToString(),
+                            employeeName = row["employeeName"].ToString(),
+                            phone = row["phone"].ToString(),
+                            address = row["address"].ToString(),
+                            job = row["job"].ToString(),
+                            position = row["position"].ToString(),
+                            email = row["email"].ToString(),
+                            salary = Convert.ToDouble(row["salary"])
+                        };
+
+                        result.Add(employee);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- StufoodSystem_ADMIN/SEFinalProject_Winform/Views/fmMain.cs
+++ StufoodSystem_ADMIN/SEFinalProject_Winform/Views/fmMain.cs
@@ -1,8 +1,12 @@
 ﻿using MaterialSkin.Controls;
+using StufoodSystem_ADMIN.Controllers;
+using StufoodSystem_ADMIN.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -14,6 +18,8 @@ namespace StufoodSystem_ADMIN.Views
     public partial class fmMain : MaterialForm
     {
         readonly MaterialSkin.MaterialSkinManager materialSkinManager;
+        String strConn = ConfigurationManager.ConnectionStrings["MySQLConnection"].ConnectionString;
+        SqlConnection conn;
         public fmMain()
         {
             InitializeComponent();
@@ -25,5 +31,35 @@ namespace StufoodSystem_ADMIN.Views
                 MaterialSkin.Primary.Amber700, MaterialSkin.Primary.Amber100, MaterialSkin.Accent.Blue100,
                 MaterialSkin.TextShade.BLACK);
         }
+
+        private void materialTabSelector7_Click(object sender, EventArgs e)
+        {
+            List<Employee> employees = EmployeeController.GetAllEmployee();
+
+            employeeListView.Items.Clear();
+            employeeListView.AllowDrop = true;
+
+            foreach (Employee employee in employees)
+            {
+                ListViewItem item = new ListViewItem(employee.employeeID);
+                item.SubItems.Add(employee.employeeName);
+                item.SubItems.Add(employee.phone);
+                item.SubItems.Add(employee.address);
+                item.SubItems.Add(employee.job);
+                item.SubItems.Add(employee.position);
+                item.SubItems.Add(employee.email);
+                item.SubItems.Add(employee.salary.ToString());
+
+                employeeListView.Items.Add(item);
+            }
+        }
+
+        private void employeeListView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
     }
 }

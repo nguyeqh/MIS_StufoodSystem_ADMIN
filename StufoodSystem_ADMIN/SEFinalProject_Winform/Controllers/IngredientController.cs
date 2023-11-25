537af50e991a60ac664d02d6c219f017b3c2a034
Nguyễn Quỳnh Như <104415043+nguyeqh@users.noreply.github.com>
2023-11-25 11:23:26 +0700

    update Ingredient model and create controller

---
+++ StufoodSystem_ADMIN/SEFinalProject_Winform/Controllers/IngredientController.cs
@@ -0,0 +1,138 @@
+﻿using StufoodSystem_ADMIN.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StufoodSystem_ADMIN.Controllers
+{
+    public class IngredientController
+    {
+        private static String strConn = ConfigurationManager.ConnectionStrings["MySQLConnection"].ConnectionString;
+        private static SqlConnection conn;
+        public IngredientController() 
+        {
+            conn = new SqlConnection(strConn);
+            
+        }
+        public static List<Ingredient> GetAllIgredient()
+        {
+            conn = new SqlConnection(strConn);
+            conn.Open();
+            String sSQL = "SELECT * FROM Ingredient";
+            List<Ingredient> ingredients = GetIngredientsFromDatabase(strConn, sSQL);
+      
+            conn.Close();
+            return ingredients;
+
+        }
+
+        public static Ingredient GetIngredientByID(String id)
+        {
+            String sSQL = "SELECT * FROM Ingredient;";
+
+            List<Ingredient> ingredients = GetIngredientsFromDatabase(strConn, sSQL);
+            Ingredient foundItem = ingredients.FirstOrDefault(item => item.ingredientID == id);
+
+
+            return foundItem;
+        }
+
+        public static void UpdateIngredient (String id, Ingredient updateIngredient)
+        {
+            String sSQL = "UPDATE Ingredient SET IngredientID = @IngredientID, IngredientName = @IngredientName, IngredientDescription = @IngredientDescription, IngredientCategory = @IngredientCategory, IngredientPreservation = @IngredientPreservation, Price = @Price, QuantityAvailable = @QuantityAvailable";
+
+
+            SqlConnection conn = new SqlConnection(strConn);
+
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(sSQL, conn);
+            cmd.Parameters.Add(new SqlParameter("@IngredientID", updateIngredient.ingredientID));
+            cmd.Parameters.Add(new SqlParameter("@IngredientName", updateIngredient.ingredientName));
+            cmd.Parameters.Add(new SqlParameter("@IngredientDescription", updateIngredient.ingredientDescription));
+            cmd.Parameters.Add(new SqlParameter("@IngredientCategory", updateIngredient.ingredientCategory));
+            cmd.Parameters.Add(new SqlParameter("@IngredientPreservation", updateIngredient.ingredientPreservation));
+            cmd.Parameters.Add(new SqlParameter("@Price", updateIngredient.price));
+            cmd.Parameters.Add(new SqlParameter("@QuantityAvailable", updateIngredient.quantityAvailable));
+            try
+            {
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error:" + ex.Message);
+            }
+        }
+
+        public static void CreateIngredient(Ingredient ingredient)
+        {
+
+            String sSQL = "INSERT INTO Ingredient (IngredientID, IngredientName, IngredientDescription, IngredientCategory, IngredientPreservation, Price, QuantityAvailable)" +
+                "VALUES (@IngredientID, @IngredientName, @IngredientDescription, @IngredientCategory, @IngredientPreservation, @Price, @QuantityAvailable);";
+            SqlConnection conn = new SqlConnection(strConn);
+
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(sSQL, conn);
+            cmd.Parameters.Add(new SqlParameter("@IngredientID", ingredient.ingredientID));
+            cmd.Parameters.Add(new SqlParameter("@IngredientName", ingredient.ingredientName));
+            cmd.Parameters.Add(new SqlParameter("@IngredientDescription", ingredient.ingredientDescription));
+            cmd.Parameters.Add(new SqlParameter("@IngredientCategory", ingredient.ingredientCategory));
+            cmd.Parameters.Add(new SqlParameter("@IngredientPreservation", ingredient.ingredientPreservation));
+            cmd.Parameters.Add(new SqlParameter("@Price", ingredient.price));
+            cmd.Parameters.Add(new SqlParameter("@QuantityAvailable", ingredient.quantityAvailable));
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error:" + ex.Message);
+            }
+        
+        }
+        //--------------- PRIVATE FUNCTION -------------------- //
+
+        static List<Ingredient> GetIngredientsFromDatabase(string connectionString, string sSQL)
+        {
+            List<Ingredient> result = new List<Ingredient>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SqlDataAdapter adapter = new SqlDataAdapter(sSQL, connection))
+                {
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+
+                    // Convert DataTable rows to List<Employee>
+                    foreach (DataRow row in dataTable.Rows)
+                    {
+                        Ingredient ingredient = new Ingredient
+                        {
+                            ingredientID = row["ingredientID"].ToString(),
+                            ingredientCategory = row["ingredientCategory"].ToString(),
+                            ingredientDescription = row["ingredientDescription"].ToString(),
+                            ingredientName = row["ingredientName"].ToString(),
+                            ingredientPreservation = row["ingredientPreservation"].ToString(),
+                            quantityAvailable = Convert.ToInt32(row["quantityAvailable"]),
+                            price = Convert.ToDouble(row["price"]),
+
+                        };
+
+                        result.Add(ingredient);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- StufoodSystem_ADMIN/SEFinalProject_Winform/Models/Ingredient.cs
+++ StufoodSystem_ADMIN/SEFinalProject_Winform/Models/Ingredient.cs
@@ -11,7 +11,7 @@ namespace StufoodSystem_ADMIN.Models
         public string ingredientID { get; set; } = "";
         public string ingredientName { get; set; } = "";
         public string ingredientCategory { get; set; } = "";
-        public string ingredientDescription { get; } = "";
+        public string ingredientDescription { get; set; } = "";
         public string ingredientPreservation { get; set; } = "";
         public int quantityAvailable { get; set; } = 0;
         public double price { get; set; } = 0.0;
